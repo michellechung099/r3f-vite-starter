6b87807523e7a7b4438c55cadbeba18582f31d90
michellechung099 <mchung099@gmail.com>
2024-06-14 11:07:18 -0700

    fix orbit controls

--- src/components/BasicScene.jsx
+++ src/components/BasicScene.jsx
@@ -3,12 +3,11 @@ import { OrbitControls } from "@react-three/drei";
 export const BasicScene = () => {
   return (
     <>
-      <OrbitControls>
-        <mesh>
-          <boxGeometry />
-          <meshNormalMaterial />
-        </mesh>
-      </OrbitControls>
+      <OrbitControls />
+      <mesh>
+        <boxGeometry />
+        <meshNormalMaterial />
+      </mesh>
     </>
   );
 };
